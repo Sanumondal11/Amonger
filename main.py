84ba38bde65a68fe87d9fd91db92b626dbbfca33
Bowser <sanumondal093@gmail.com>
2025-03-10 22:32:26 +0530

    screen is coming

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import os
 import random
-import pygame as py
+import pygame
 import math
 from os import listdir
 from os.path import isfile,join
-py.init()
+pygame.init()
 
-py.display.set_caption("Amonger")
+pygame.display.set_caption("Amonger")
 
 WIDTH,HEIGHT=1000,800
 FPS=60
 PLAYER_VEL=3
 
-window = py.display.set_mode((WIDTH,HEIGHT))
+window = pygame.display.set_mode((WIDTH,HEIGHT))
 
 # background
 def get_background(name):
-    image = py.image.load(join("assests","Background",name))
+    image = pygame.image.load(join("assets","Background",name))
     _,_,width,height=image.get_rect()
     tiles=[]
     for i in range(WIDTH//width+1):
@@ -29,24 +29,24 @@ def draw(window,background,bg_image):
     for tile in background:
         window.blit(bg_image,tuple(tile)) 
 
-    py.display.update()
+    pygame.display.update()
 
 def main(window):
-    clock=py.time.Clock()
+    clock=pygame.time.Clock()
     background,bg_image=get_background("Green.png")
     run=True
     while run:
         clock.tick(FPS)
 
-        for event in py.event.get():
-            if event.type == py.QUIT:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 run = False
                 break
 
         draw(window,background,bg_image)
 
-    py.quit()
+    pygame.quit()
     quit()
 
-if "__name__"=="__main__":
+if __name__=="__main__":
     main(window) 
